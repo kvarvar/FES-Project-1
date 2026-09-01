756d2d4c684670bbd1a753f2fc83654e1ccc57f5
kvarvar <katherine.adjodha@gmail.com>
2026-09-01 14:11:29 -0400

    js email started

---
+++ exclusive-eportfolio/index.js
@@ -0,0 +1,11 @@
+// template_q2q3sei
+// service_xhs4peb
+// pr1xc52OEieIK2e0x
+
+function contact(event) {
+    event.preventDefault();
+
+    setTimeout(() => {
+        console.log('it worked')
+    }, 500);
+}
