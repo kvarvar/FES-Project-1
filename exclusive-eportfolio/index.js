cd559a3b78f9d3d907c28c72be8d3282714a1552
kvarvar <katherine.adjodha@gmail.com>
2026-09-03 13:46:36 -0400

    email function js completed

--- exclusive-eportfolio/index.js
+++ exclusive-eportfolio/index.js
@@ -3,9 +3,25 @@
 // pr1xc52OEieIK2e0x
 
 function contact(event) {
-    event.preventDefault();
-
-    setTimeout(() => {
-        console.log('it worked')
-    }, 500);
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_xhs4peb",
+      "template_q2q3sei",
+      event.target,
+      "pr1xc52OEieIK2e0x",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "the email service is temporarily unavailable. Please contact me directly on katherine.adjodha@gmail.com",
+      );
+    });
 }
