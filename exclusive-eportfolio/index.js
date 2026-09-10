69c4d560e4544ab25d293c964a1dc7e386bec83f
kvarvar <katherine.adjodha@gmail.com>
2026-09-10 12:03:02 -0400

    shape background java completed

--- exclusive-eportfolio/index.js
+++ exclusive-eportfolio/index.js
@@ -1,7 +1,31 @@
 // template_q2q3sei
 // service_xhs4peb
 // pr1xc52OEieIK2e0x
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(" .shape");
+  const X = event.clientX * scaleFactor;
+  const Y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${X * boolInt}px, ${Y * boolInt}px)`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  }
+  else {
+    document.body.classList.remove("dark-theme");
+  }
+}
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
@@ -25,7 +49,7 @@ function contact(event) {
       );
     });
 }
-let isModalOpen = false;
+
 function toggleModal (){
   if (isModalOpen) {
     isModalOpen = false;
